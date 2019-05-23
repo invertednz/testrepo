db770acd68bc73655e3f5f20cecd8583b1f3da78
invertednz <jamesfarrier@gmail.com>
2019-05-23 15:48:38 -0700

    fix bug

--- PushResultsToAppsurify.sh
+++ PushResultsToAppsurify.sh
@@ -20,7 +20,7 @@ echo $report"*.xml\`"
 for fileName in `ls -1 "$report*.xml"`
 	  do
 	    echo "call api for $fileName" 
-		import=`curl -X POST "$url/api/external/import/" -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' -H "token: $apiKey" -F "file=@$fileName" -F 'project_name'=$project -F 'test_suite_name'=$testsuite -F 'type'=$importtype -F 'commit'=$commitId` ; fi
+		import=`curl -X POST "$url/api/external/import/" -H 'content-type: multipart/form-data; boundary=----WebKitFormBoundary7MA4YWxkTrZu0gW' -H "token: $apiKey" -F "file=@$fileName" -F 'project_name'=$project -F 'test_suite_name'=$testsuite -F 'type'=$importtype -F 'commit'=$commitId`
 		echo $import
 		#get testrun id
 		var=`echo $import | sed 's/\\\\\//\//g' | sed 's/[{}]//g' |tr "," "\n" | sed 's/\"\:\"/\|/g' | sed 's/[\,]/ /g' | sed 's/\"//g' |  sed '10p;d' `
